781649f7373ecb6f175b208e04436219abe13db3
johnson.andrew.g@gmail.com <johnson.andrew.g@gmail.com@2e6cc20c-539c-a1c1-b58e-2cffae58dd93>
2011-05-04 03:56:47 +0000

    -marble moves with wsad keys (arrows move camera w/ default navigator) -marble won't go through any walls -gravity should work (starts in middle, is on bottom when i find it)

--- trunk/src/Cube.java
+++ trunk/src/Cube.java
@@ -9,7 +9,7 @@ import javax.vecmath.*;
  */
 public class Cube {
 
-    private static final float BOXDIM = 10.0f;  //actually half of side length
+    protected static final float BOXDIM = 10.0f;  //actually half of side length
     private BranchGroup boxBG;
 
     public Cube(){
@@ -24,42 +24,42 @@ public class Cube {
 
         //cube sides
 
-        Box bottom = new Box(BOXDIM,.1f,BOXDIM,appB);
+        Box bottom = new Box(BOXDIM,.001f,BOXDIM,appB);
         Transform3D t1 = new Transform3D();
         t1.set(new Vector3d(0, -BOXDIM, 0));
         TransformGroup tg1 = new TransformGroup(t1);
         boxBG.addChild(tg1);
         tg1.addChild(bottom);
 
-        Box top = new Box(BOXDIM,.1f,BOXDIM,appB);
+        Box top = new Box(BOXDIM,.001f,BOXDIM,appB);
         Transform3D t2 = new Transform3D();
         t2.set(new Vector3d(0, BOXDIM, 0));
         TransformGroup tg2 = new TransformGroup(t2);
         boxBG.addChild(tg2);
         tg2.addChild(top);
 
-        Box front = new Box(BOXDIM,BOXDIM,0.1f,appB);
+        Box front = new Box(BOXDIM,BOXDIM,0.001f,appB);
         Transform3D t3 = new Transform3D();
         t3.set(new Vector3d(0, 0, BOXDIM));
         TransformGroup tg3 = new TransformGroup(t3);
         boxBG.addChild(tg3);
         tg3.addChild(front);
 
-        Box back = new Box(BOXDIM,BOXDIM,0.1f,appB);
+        Box back = new Box(BOXDIM,BOXDIM,0.001f,appB);
         Transform3D t4 = new Transform3D();
         t4.set(new Vector3d(0, 0, -BOXDIM));
         TransformGroup tg4 = new TransformGroup(t4);
         boxBG.addChild(tg4);
         tg4.addChild(back);
 /*
-        Box left = new Box(.1f,BOXDIM,BOXDIM,appB);
+        Box left = new Box(.001f,BOXDIM,BOXDIM,appB);
         Transform3D t5 = new Transform3D();
         t5.set(new Vector3d(-BOXDIM, 0, 0));
         TransformGroup tg5 = new TransformGroup(t5);
         boxBG.addChild(tg5);
         tg5.addChild(left);
 
-        Box right = new Box(.1f,BOXDIM,BOXDIM,appB);
+        Box right = new Box(.001f,BOXDIM,BOXDIM,appB);
         Transform3D t6 = new Transform3D();
         t6.set(new Vector3d(BOXDIM, 0, 0));
         TransformGroup tg6 = new TransformGroup(t6);
