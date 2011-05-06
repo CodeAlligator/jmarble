82c67d3ab1ce2ac82332096f6d536214293bb74d
johnson.andrew.g@gmail.com <johnson.andrew.g@gmail.com@2e6cc20c-539c-a1c1-b58e-2cffae58dd93>
2011-05-06 06:56:36 +0000

    -added a lot of gravity/vector stuff that almost but didn't work: it's included but commented out. -additional keyboard buttons change gravity for testing

--- src/Ball.java
+++ src/Ball.java
@@ -12,11 +12,15 @@ public class Ball {
     private static final float RADIUS = .2f;
     TransformGroup posTG; //A transform to the current position
     TransformGroup rotTG; //A transform to the current direction
+    //TransformGroup look;
     Transform3D t3d;
     Transform3D rot3d;
+    //Transform3D xform;
     Vector3f velocity;  //Current velocity of the ball
     Vector3f gravvelocity;  //Current velocity of the ball due to gravity
     Vector3f position;  //Current position of the ball
+    Vector3f gravity;   //gravity vector from main method
+    Vector3f pointing;  //vector pointing in balls direction
     float direction;    //Curent direction the ball is facing
     boolean keyup = false;
     boolean keydown = false;
@@ -27,12 +31,15 @@ public class Ball {
         ballBG = new BranchGroup();
         posTG = new TransformGroup();
         rotTG = new TransformGroup();
+        //look = new TransformGroup();
 
         // Enable the TRANSFORM_WRITE capability so can modify position
         posTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         posTG.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
         rotTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         rotTG.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+        //look.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        //look.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 
         //set appearance
         Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
@@ -47,14 +54,22 @@ public class Ball {
         position  = new Vector3f(0.0f, 4.0f, 0.0f);
         velocity  = new Vector3f(0.0f, 0.0f, 0.0f);
         gravvelocity  = new Vector3f(0.0f, 0.0f, 0.0f);
+        gravity = new Vector3f(0.0f, -1.0f, 0.0f);
+        pointing = new Vector3f(0.0f, 0.0f, -1.0f);
         setPositionTG();
         setDirectionTG();
+        //createTransform();
+
 
         //create ball
         Sphere s = new Sphere(RADIUS, app);
         rotTG.addChild(s);
         posTG.addChild(rotTG);
+        //posTG.addChild(s);
         ballBG.addChild(posTG);
+        //look.addChild(s);
+        //ballBG.addChild(look);
+
 
         /*TODO
          *
@@ -84,16 +99,31 @@ public class Ball {
         rot3d.rotY(direction);
         rotTG.setTransform(rot3d);
     }
+
+//    void createTransform(){
+//        xform = new Transform3D();
+//        Point3d eye = new Point3d(position.x, position.y, position.z);
+//        Point3d center = new Point3d(pointing.x, pointing.y, pointing.z);
+//        Vector3d grav = new Vector3d(gravity);
+//        grav.scale(-1.0);
+//        xform.lookAt(eye, center, grav);
+//        //look.setTransform(xform);
+//        setPositionTG();
+//    }
     
     void updateBall(float dt, Vector3f gravDir)
     {
         // dt is seconds elapsed time since previous frame
+        gravity = gravDir;
+        gravity.normalize();
+
         if (keyup)
         {
             velocity.z = (float) (Math.cos(direction) * -MOVESPEED * dt);
             velocity.x = (float) (Math.sin(direction) * -MOVESPEED * dt);
             velocity.y = 0;
             position.add(velocity);
+            //position.scaleAdd(MOVESPEED * dt, pointing, position);
         }
 
         if (keydown)
@@ -102,6 +132,7 @@ public class Ball {
             velocity.x = (float) (Math.sin(direction) * MOVESPEED * dt);
             velocity.y = 0;
             position.add(velocity);
+            //position.scaleAdd(-MOVESPEED * dt, pointing, position);
         }
         if (keyleft)
         {
@@ -109,6 +140,11 @@ public class Ball {
             if (direction > 2.0f*Math.PI);
                     direction -= 2.0f*Math.PI;
             setDirectionTG();
+//            Vector3f a = new Vector3f();
+//            a.cross(pointing, gravity);
+//            pointing.scaleAdd(1.0f*dt, a, pointing);
+//            pointing.normalize();
+//            direction = (float) Math.asin(pointing.x);
         }
         if (keyright)
         {
@@ -116,11 +152,16 @@ public class Ball {
             if (direction < 0.0f);
                     direction += 2.0f*Math.PI;
             setDirectionTG();
+//            Vector3f a = new Vector3f();
+//            a.cross(gravity, pointing);
+//            pointing.scaleAdd(1.0f*dt, a, pointing);
+//            pointing.normalize();
         }
         
         //cumulativly add gravity speed
-        gravvelocity.scaleAdd(dt*GRAVSPEED, gravDir, gravvelocity);
-        position.add(gravvelocity);
+        //gravvelocity.scaleAdd(dt*GRAVSPEED, gravDir, gravvelocity);
+        //position.add(gravvelocity);
+        position.scaleAdd(dt*GRAVSPEED, gravity, position);
 
         //dont fall through walls
         if (position.y < -Cube.BOXDIM + RADIUS){
@@ -150,5 +191,6 @@ public class Ball {
 
         setPositionTG();
 
+        //createTransform();
     }
 }
