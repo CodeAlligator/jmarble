77068d2a177b208543738a6125595d890a18f331
robopaul9@gmail.com <robopaul9@gmail.com@2e6cc20c-539c-a1c1-b58e-2cffae58dd93>
2011-05-08 23:31:09 +0000

    - Multiple coins are created in world. - Player can now collect a coin by colliding with it. - When collected, a println statement informs player that coin was collected - When all coins collected, a println statement informs player he won. - Renamed main class from Marble to JMarble to avoid confusion of ball vs. marble.

--- src/Coin.java
+++ src/Coin.java
@@ -3,74 +3,43 @@ import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Material;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.AxisAngle4d;
 import javax.vecmath.Color3f;
 import javax.vecmath.Vector3d;
+import javax.vecmath.Vector3f;
 
 import com.sun.j3d.utils.geometry.Box;
-
+import com.sun.j3d.utils.geometry.Cylinder;
 
 /**
  *
- * @author Andrew
+ * @author Paul
  */
 public class Coin {
-    protected static final float BOXDIM = 1.0f;  //actually half of side length
     private BranchGroup boxBG;
-
-    public Coin(){
-        boxBG = new BranchGroup();
+    public float x;
+    public float z;
+    
+    public Coin(float x, float z){
+    	this.x = x;
+    	this.z = z;
+    	
+    	boxBG = new BranchGroup();
 
         //set appearance
         Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
         Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
-        Color3f blue = new Color3f(0.1f, 0.2f, 1.0f);
+        Color3f green = new Color3f(0.9f, 1.0f, 0.1f);
         Appearance appB = new Appearance();
-        appB.setMaterial(new Material(blue, black, blue, white, 80.0f));
-
-        //cube sides
-
-        Box bottom = new Box(BOXDIM,.001f,BOXDIM,appB);
+        appB.setMaterial(new Material(green, black, green, white, 80.0f));
+        
+        Cylinder bottom = new Cylinder(.3f,.2f, appB);
         Transform3D t1 = new Transform3D();
-        t1.set(new Vector3d(0, -BOXDIM, 0));
+        t1.set(new Vector3d(x, -10f, z));
         TransformGroup tg1 = new TransformGroup(t1);
         boxBG.addChild(tg1);
         tg1.addChild(bottom);
 
-        Box top = new Box(BOXDIM,.001f,BOXDIM,appB);
-        Transform3D t2 = new Transform3D();
-        t2.set(new Vector3d(0, BOXDIM, 0));
-        TransformGroup tg2 = new TransformGroup(t2);
-        boxBG.addChild(tg2);
-        tg2.addChild(top);
-
-        Box front = new Box(BOXDIM,BOXDIM,0.001f,appB);
-        Transform3D t3 = new Transform3D();
-        t3.set(new Vector3d(0, 0, BOXDIM));
-        TransformGroup tg3 = new TransformGroup(t3);
-        boxBG.addChild(tg3);
-        tg3.addChild(front);
-
-        Box back = new Box(BOXDIM,BOXDIM,0.001f,appB);
-        Transform3D t4 = new Transform3D();
-        t4.set(new Vector3d(0, 0, -BOXDIM));
-        TransformGroup tg4 = new TransformGroup(t4);
-        boxBG.addChild(tg4);
-        tg4.addChild(back);
-/*
-        Box left = new Box(.001f,BOXDIM,BOXDIM,appB);
-        Transform3D t5 = new Transform3D();
-        t5.set(new Vector3d(-BOXDIM, 0, 0));
-        TransformGroup tg5 = new TransformGroup(t5);
-        boxBG.addChild(tg5);
-        tg5.addChild(left);
-
-        Box right = new Box(.001f,BOXDIM,BOXDIM,appB);
-        Transform3D t6 = new Transform3D();
-        t6.set(new Vector3d(BOXDIM, 0, 0));
-        TransformGroup tg6 = new TransformGroup(t6);
-        boxBG.addChild(tg6);
-        tg6.addChild(right);
-*/
         boxBG.compile();
     }
 
--- src/JMarble.java
+++ src/JMarble.java
@@ -7,6 +7,10 @@
  * 5/6/2011
  *
  * Uses code from Mike Slattery's MBall
+ *
+ * The game:
+ * 	You are a marble that rolls around the world. Use the classic AWSD keys to control
+ * which direction to roll in. Collect coins by rolling into them.
  */
 
 import java.applet.Applet;
@@ -18,13 +22,15 @@ import javax.media.j3d.*;
 import javax.vecmath.*;
 import com.sun.j3d.utils.behaviors.keyboard.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Enumeration;
 
-public class Marble extends Applet{
+public class JMarble extends Applet{
 
     SimpleUniverse simpleU=null;
     Vector3f gravity;  //direction of marble acceleration and world position
     Ball player;
+    ArrayList<Coin> coins = new ArrayList<Coin>();
     Transform3D followT3D;
 
     // main drawing section
@@ -34,11 +40,12 @@ public class Marble extends Applet{
 
         //Ambient light
         AmbientLight lightA = new AmbientLight();
-        BoundingSphere bs = new BoundingSphere(new Point3d(0.0,0.0,0.0),100.0);
+        BoundingSphere bs = new BoundingSphere(new Point3d(0.0,0.0,0.0), 1000);
         lightA.setInfluencingBounds(bs);
+        lightA.setColor(new Color3f(1.0f, 0.1f, 0.7f));
         objRoot.addChild(lightA);
 
-        // Directional light
+        // Directional white light
         DirectionalLight lightD = new DirectionalLight();
         lightD.setInfluencingBounds(bs);
         Vector3f direction = new Vector3f(-1.0f, -1.0f, -1.0f);
@@ -47,16 +54,43 @@ public class Marble extends Applet{
         lightD.setColor(new Color3f(1f, 1f, 1f));
         objRoot.addChild(lightD);
 
+        //	Directional colored light
+        DirectionalLight lightE = new DirectionalLight();
+        lightE.setInfluencingBounds(bs);
+        Vector3f direction2 = new Vector3f(0.4f, -0.1f, -1.0f);
+        direction2.normalize();
+        lightE.setDirection(direction2);
+        lightE.setColor(new Color3f(1f, .7f, .3f));
+        objRoot.addChild(lightE);
+
         // Set the background sky to blue
         Background bg = new Background();
         bg.setColor(0.0f, 0.0f, 1.0f); // yellow
         bg.setApplicationBounds(new BoundingSphere());
         objRoot.addChild(bg);
 
+        //	Create the coins
+        coins.add(new Coin(.9f,.2f));
+        coins.add(new Coin(1.3f,-3.2f));
+        coins.add(new Coin(-2.3f,2.2f));
+        coins.add(new Coin(3.3f,1.2f));
+        coins.add(new Coin(-2.3f,-2.2f));
+        coins.add(new Coin(-3.3f,1.2f));
+
         // Place Box and Marble
         player = new Ball();
         objRoot.addChild(new Cube().getBG());
+
+        //	Add the coins
+        for(int i = 0; i < 6; i++){
+        	objRoot.addChild(coins.get(i).getBG());
+        }
+
+        //	Add the player
         objRoot.addChild(player.getBG());
+
+        objRoot.addChild(new ColorCube(2.3));
+
 
         /*TODO
          * draw sky and ground based on gravity vector
@@ -65,10 +99,9 @@ public class Marble extends Applet{
          * add keyboard controls to move ball
          * add update method and/or transform group to slowly change gravity vector
          */
-        
-        //	Place coins
-        objRoot.addChild(new Coin().getBG());
-        
+
+
+
         gravity = new Vector3f(0.0f, -1.0f, 0.0f);
 
         // Create a new Behavior object to update each frame
@@ -76,12 +109,12 @@ public class Marble extends Applet{
         BoundingSphere bounds = new BoundingSphere(new Point3d(),100.0);
         cf.setSchedulingBounds(bounds);
         objRoot.addChild(cf);
-         
 
-	return objRoot;
+
+        return objRoot;
     } // end of CreateSceneGraph method
 
-    
+
     class ComputeFrame extends Behavior
     {
 
@@ -128,15 +161,53 @@ public class Marble extends Applet{
                 case KeyEvent.VK_F: gravity = new Vector3f(0.0f, 0.0f, -1.0f);; break;//front
                 case KeyEvent.VK_B: gravity = new Vector3f(0.0f, 0.0f, 1.0f);; break;//back
             }
+
+            //	Check if player hit coin
+            if(coins.size() > 0 && (player.position.x > coins.get(0).x - .4f) && (player.position.x < coins.get(0).x + .4f) && (player.position.z > coins.get(0).z - .4f) && (player.position.z < coins.get(0).z + .4f)){
+        		System.out.println("Collected a coin");
+        		coins.remove(0);
+        	}
+            if(coins.size() > 1 && (player.position.x > coins.get(1).x - .4f) && (player.position.x < coins.get(1).x + .4f) && (player.position.z > coins.get(1).z - .4f) && (player.position.z < coins.get(1).z + .4f)){
+        		System.out.println("Collected a coin");
+        		coins.remove(1);
+        	}
+            if(coins.size() > 2 && (player.position.x > coins.get(2).x - .4f) && (player.position.x < coins.get(2).x + .4f) && (player.position.z > coins.get(2).z - .4f) && (player.position.z < coins.get(2).z + .4f)){
+        		System.out.println("Collected a coin");
+        		coins.remove(2);
+        	}
+            if(coins.size() > 3 && (player.position.x > coins.get(3).x - .4f) && (player.position.x < coins.get(3).x + .4f) && (player.position.z > coins.get(3).z - .4f) && (player.position.z < coins.get(3).z + .4f)){
+        		System.out.println("Collected a coin");
+        		coins.remove(3);
+        	}
+            if(coins.size() > 4 && (player.position.x > coins.get(4).x - .4f) && (player.position.x < coins.get(4).x + .4f) && (player.position.z > coins.get(4).z - .4f) && (player.position.z < coins.get(4).z + .4f)){
+        		System.out.println("Collected a coin");
+        		coins.remove(4);
+        	}
+            if(coins.size() > 5 && (player.position.x > coins.get(5).x - .4f) && (player.position.x < coins.get(5).x + .4f) && (player.position.z > coins.get(5).z - .4f) && (player.position.z < coins.get(5).z + .4f)){
+        		System.out.println("Collected a coin");
+        		coins.remove(5);
+        	}
+
+        	if(coins.size() == 0){
+				System.out.println("You Won! Game Over!");
+			}
         }
 
         public void keyReleased(KeyEvent e) {
             int key = e.getKeyCode();
             switch (key) {
-                case KeyEvent.VK_W: player.keyup = false; break;
-                case KeyEvent.VK_S: player.keydown = false; break;
-                case KeyEvent.VK_D: player.keyright = false; break;
-                case KeyEvent.VK_A: player.keyleft = false; break;
+                case KeyEvent.VK_W:	//	forward
+                	player.keyup = false;
+                	break;
+                case KeyEvent.VK_S:	//	back
+                	player.keydown = false;
+                	break;
+                case KeyEvent.VK_D:	//	right
+                	player.keyright = false;
+                	break;
+                case KeyEvent.VK_A:	//	left
+                	player.keyleft = false;
+                	break;
             }
         }
     }
@@ -159,12 +230,12 @@ public class Marble extends Applet{
         vpTrans.setTransform(T3D);
     }
 
-    public Marble() {
+    public JMarble() {
         setLayout(new BorderLayout());
         Canvas3D canvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
         add("Center", canvas3D);
-        
-        
+
+
         canvas3D.addKeyListener(new keyL());
 
         // SimpleUniverse is a Convenience Utility class
@@ -175,11 +246,11 @@ public class Marble extends Applet{
         //set up a KeyNavigator to let us move about
         TransformGroup vpTrans = simpleU.getViewingPlatform().getViewPlatformTransform();
 
-        
+
 //        KeyNavigatorBehavior keyNav = new KeyNavigatorBehavior(vpTrans);
 //        keyNav.setSchedulingBounds(new BoundingSphere(new Point3d(), 1000.0));
 //        scene.addChild(keyNav);
-        
+
 
 	scene.compile();
 
@@ -199,7 +270,7 @@ public class Marble extends Applet{
     //  The following allows this to be run as an application
     //  as well as an applet
     public static void main(String[] args) {
-        Frame frame = new MainFrame(new Marble(), 256, 256);
+        Frame frame = new MainFrame(new JMarble(), 256, 256);
         frame.setSize(800, 600);
     }
 }
